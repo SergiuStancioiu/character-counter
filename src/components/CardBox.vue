6cf1a551c1738708b3a5fe3d8bf5902f3bd2ec07
Sergiu Stancioiu <s.sergiu96@gmail.com>
2025-12-26 15:21:01 +0200

    Created CardBox and TextContent components

---
+++ src/components/CardBox.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps({
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  backgroundImage: { type: String, required: true },
+  backgroundColor: { type: String, required: true },
+});
+
+const imageUrl = new URL(
+  `../assets/images/${props.backgroundImage}`,
+  import.meta.url
+).href;
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-between overflow-hidden rounded-xl pl-2.5 max-h-32.5"
+    :style="{ backgroundColor }"
+  >
+    <div class="flex flex-col gap-2.5">
+      <h2 class="text-3xl font-medium">278</h2>
+      <p class="text-xl">{{ props.name }}</p>
+    </div>
+    <div class="-mr-13">
+      <img class="w-full" :src="imageUrl" alt="Background Image" />
+    </div>
+  </div>
+</template>
--- src/components/TextContent.vue
+++ src/components/TextContent.vue
@@ -1,5 +1,27 @@
 <script setup lang="ts">
 import Checkbox from './Checkbox.vue';
+import CardBox from './CardBox.vue';
+
+const cardBoxArr = [
+  {
+    id: 'total-characters',
+    name: 'Total Characters',
+    backgroundImage: 'pattern-character-count.svg',
+    backgroundColor: '#d3a1fa',
+  },
+  {
+    id: 'word-count',
+    name: 'Word Count',
+    backgroundImage: 'pattern-word-count.svg',
+    backgroundColor: '#ff9f00',
+  },
+  {
+    id: 'sentence-count',
+    name: 'Sentence Count',
+    backgroundImage: 'pattern-sentence-count.svg',
+    backgroundColor: '#ff8159',
+  },
+];
 </script>
 
 <template>
@@ -12,7 +34,7 @@ import Checkbox from './Checkbox.vue';
       </h1>
       <div>
         <textarea
-          class="w-full border border-gray-200 bg-gray-100 pl-2.5 pt-2.5 h-50 rounded-md"
+          class="w-full border-textarea-border-light border-2 bg-textarea-background-light pl-2.5 pt-2.5 h-50 rounded-md"
           name="analyze-text"
           id="analyze-text"
           placeholder="Start typing here...(or paste your text)"
@@ -31,6 +53,18 @@ import Checkbox from './Checkbox.vue';
         </div>
       </div>
     </div>
-    <div class="dark:text-white">Approx. reading time: &lt;1 minute</div>
+    <div class="dark:text-whit pb-11.25">
+      Approx. reading time: &lt;1 minute
+    </div>
+    <div class="flex flex-col gap-4 mb-8">
+      <div v-for="cardBox in cardBoxArr" :key="cardBox.id">
+        <CardBox
+          :id="cardBox.id"
+          :name="cardBox.name"
+          :backgroundImage="cardBox.backgroundImage"
+          :backgroundColor="cardBox.backgroundColor"
+        />
+      </div>
+    </div>
   </div>
 </template>
